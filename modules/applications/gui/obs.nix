9a805a6bd85ffb4463072a36896dd96b80e08c06
Xavier Lauzon <xavier@lumae.net>
2024-08-14 11:56:47 -0400

    use wrapOBS to make plugins functional

--- modules/applications/gui/obs.nix
+++ modules/applications/gui/obs.nix
@@ -19,14 +19,18 @@ in
     home = {
       packages = with pkgs;
         [
-          obs-studio
-          obs-studio-plugins.wlrobs
-          obs-studio-plugins.obs-vaapi
-          obs-studio-plugins.obs-vkcapture
-          obs-studio-plugins.obs-gstreamer
-          obs-studio-plugins.obs-pipewire-audio-capture
-
+          (wrapOBS {
+            plugins = with obs-studio-plugins; [
+              wlrobs
+              obs-vaapi
+              obs-vkcapture
+              obs-gstreamer
+              obs-pipewire-audio-capture
+            ];
+          })
+          v4l-utils
         ];
+
     };
   };
 }
