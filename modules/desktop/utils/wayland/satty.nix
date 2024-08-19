e8075234d7230aaba9801b008009738f75c8c8ed
Xavier Lauzon <xavier@lumae.net>
2024-08-19 16:55:32 -0400

    use hyprshot instead of grim and slurp for screenshotting, rebind to print screen

--- modules/desktop/utils/wayland/satty.nix
+++ modules/desktop/utils/wayland/satty.nix
@@ -26,8 +26,10 @@ in
     wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
       settings = {
         #"SUPER_SHIFT, S, exec, pkill satty || hyprshot -s -r -m region | satty  -f -"
+        #"SUPER_SHIFT, S, exec, pkill satty || grim -c -g \"$(${pkgs.slurp}/bin/slurp)\" - | satty -f -"
         bind = [
-          "SUPER_SHIFT, S, exec, pkill satty || grim -g \"$(${pkgs.slurp}/bin/slurp)\" - | satty -f -"
+          "SUPER, Print, exec, hyprshot -s -r -z -m region - | satty -f -"
+          "SUPER_SHIFT, Print, exec, hyprshot -s -r -z -m active -m window - | satty -f -"
         ];
         windowrulev2 = [
           "float,class:^(com.gabm.satty)$"
