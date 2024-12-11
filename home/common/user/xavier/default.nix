65aa050f2b60d4a13345ef752cab66e36b62ef99
Xavier Lauzon <xavier@lumae.net>
2024-12-10 20:38:25 -0500

    gh username

--- home/common/user/xavier/default.nix
+++ home/common/user/xavier/default.nix
@@ -1,8 +1,16 @@
-{lib, ...}:
+{ config, inputs, lib, pkgs, specialArgs, ... }:
 
 with lib;
 {
   imports = [
     ./secrets
   ];
+
+  config = {
+    programs = {
+      git = {
+        username = "Xavier Lauzon"
+      };
+    };
+  };
 }
