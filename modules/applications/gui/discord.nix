8a9136c343cf66d2ccad808b7646695d43b5ac58
Xavier Lauzon <xavier@lumae.net>
2025-02-18 13:17:58 -0500

    change installed discord packages + Krisp patches

--- modules/applications/gui/discord.nix
+++ modules/applications/gui/discord.nix
@@ -2,6 +2,27 @@
 
 let
   cfg = config.host.home.applications.discord;
+  krisp-patcher =
+    pkgs.writers.writePython3Bin "krisp-patcher"
+      {
+        libraries = with pkgs.python3Packages; [
+          capstone
+          pyelftools
+        ];
+        flakeIgnore = [
+          "E501" # line too long (82 > 79 characters)
+          "F403" # 'from module import *' used; unable to detect undefined names
+          "F405" # name may be undefined, or defined from star imports: module
+        ];
+      }
+      (
+        builtins.readFile (
+          pkgs.fetchurl {
+            url = "https://pastebin.com/raw/8tQDsMVd";
+            sha256 = "sha256-IdXv0MfRG1/1pAAwHLS2+1NESFEz2uXrbSdvU9OvdJ8=";
+          }
+        )
+      );
 in
   with lib;
 {
@@ -19,12 +40,13 @@ in
     home = {
       packages = with pkgs;
         [
+          krisp-patcher
           vesktop
-          discord
-          discord-ptb
-          discord-canary
           arrpc
-          legcord
+          (discord.override {
+            withOpenASAR = true;
+            withVencord = true;
+          })
         ];
     };
 
@@ -32,6 +54,7 @@ in
       settings = {
         exec-once = [
           "vesktop"
+          #"discord"
         ];
       };
     };
