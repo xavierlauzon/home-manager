19b55a95e5f680ca8c8153326497c8b9c9c00c34
Xavier Lauzon <xavier@lumae.net>
2025-07-02 21:30:24 -0400

    attempt to fix hmpull

--- modules/applications/cli/bash.nix
+++ modules/applications/cli/bash.nix
@@ -79,7 +79,7 @@ in
         '';
 
         initExtra = ''
-          alias hmpull="home-manager switch --flake github:/xavierlauzon/home-manager/#$HOSTNAME.$USER $@"
+          alias hmpull="home-manager switch --flake github:xavierlauzon/home-manager/#\$HOSTNAME.\$USER --extra-experimental-features 'nix-command flakes'"
 
           if [ -d "/home/$USER/.config" ]; then alias src="cd $HOME/.config" ; fi
           if [ -d "/home/$USER/src" ]; then alias src="cd $HOME/src" ; fi
