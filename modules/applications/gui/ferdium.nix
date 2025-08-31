4aa1d808ebc715857b38fb94e98e41d9a0ef8fe8
Xavier Lauzon <xavier@lumae.net>
2025-08-31 12:23:20 -0400

    Simplify ferdium configuration

--- modules/applications/gui/ferdium.nix
+++ modules/applications/gui/ferdium.nix
@@ -26,10 +26,7 @@ in
     wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
       settings = {
         exec-once = [
-          "ferdium --ozone-platform=wayland --enable-features-WaylandWindowDecorations"
-        ];
-        windowrulev2 = [
-          "workspace 2,class:(^Ferdium)$"
+          "ferdium"
         ];
       };
     };
