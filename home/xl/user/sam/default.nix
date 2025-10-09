2f938bace68920cbb09c5e970584426f23015e1b
Xavier Lauzon <xavier@lumae.net>
2025-10-09 15:52:26 -0400

    vscode-server sucks

--- home/xl/user/sam/default.nix
+++ home/xl/user/sam/default.nix
@@ -22,13 +22,6 @@ in
       };
     };
   };
-
-  imports = [
-    "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
-  ];
-
-  services.vscode-server.enable = true;
-
   programs = {
     git = {
       userEmail = email;
